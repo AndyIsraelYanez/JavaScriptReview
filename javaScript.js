0bc40b6030c2115568da8fc0930911b8deeb1e8b
AndyIsraelYanez <andyisraeldlc@gmail.com>
2022-12-28 21:10:53 -0500

    falta acutualizar

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,26 @@
 
+// var numberOne = 10;
+// var numberTwo = 2
+
+// function suma(numberOne, numbertwo) {
+
+//     var resultado;
+//     resultado = numberOne + numbertwo;
+//     return resultado;
+
+// }
+
+// console.log(suma(numberOne, numberTwo))
+
+
+
+function suma (numberOne,numberTwo){
+
+    var numberOne = document.getElementById('numberOne');
+    var numberTwo = document.getElementById('numberTwo');
+
+
+    // var resultado;
+    // resultado = numberOne + numberTwo;
+    // return resultado;
+}
